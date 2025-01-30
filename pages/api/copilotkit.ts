9c1924ce71dddf37af747d2e2d67ec24cb2bff90
Anirban Majumder <81350357+Anirban-Majumder@users.noreply.github.com>
2025-01-30 12:00:27 +0600

    update

--- pages/api/copilotkit.ts
+++ pages/api/copilotkit.ts
@@ -7,7 +7,7 @@ import {
 
  
 
-const serviceAdapter = new GroqAdapter({ model: "llama-3.1-8b-instant" });
+const serviceAdapter = new GroqAdapter({ model: "llama-3.3-70b-versatile" });
  
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const runtime = new CopilotRuntime();
