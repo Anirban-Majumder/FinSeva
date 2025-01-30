c950d36f775f7faedb61ac6ab9a41316f15dc002
Anirban Majumder <81350357+Anirban-Majumder@users.noreply.github.com>
2025-01-30 18:46:43 +0600

    ggnt

--- layouts/head.tsx
+++ layouts/head.tsx
@@ -16,6 +16,14 @@ export const Head = () => {
         name="viewport"
       />
       <link href="/favicon.ico" rel="icon" />
+      <meta name='application-name' content='FinSeva' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content='FinSeva' />
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <link rel='apple-touch-icon' href='/icons/icon-192x192.png' />
+        <link rel='manifest' href='/manifest.json' />
     </NextHead>
   );
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js
+const withPWA = require('next-pwa');
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+ dest: 'public',
+ register: true,
+ skipWaiting: true,
+ //disable: process.env.NODE_ENV === 'development'
+})({
+ reactStrictMode: true,
+});
+
+module.exports = nextConfig;
