fa44c2aa561c520760d43c66ba7c0d50be7770fb
Anirban Majumder <81350357+Anirban-Majumder@users.noreply.github.com>
2025-01-30 17:56:39 +0600

    ui beauty

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -5,22 +5,21 @@ import { CopilotKit } from "@copilotkit/react-core";
 import { Navbar } from "@/components/navbar";
 import { ThemeProvider } from 'next-themes'
 import CopilotSideBarComponent from "@/components/ChatSideBar";
+
 export default function DefaultLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex flex-col h-screen">
+    <div className="relative flex flex-col h-screen overflow-hidden scrollbar-hide">
       <Head />
       <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
-      <CopilotKit runtimeUrl="/api/copilotkit">
-      
-      <ThemeProvider>{children}</ThemeProvider>
-      
-      <CopilotSideBarComponent />
-      </CopilotKit>
+      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-8 overflow-auto">
+        <CopilotKit runtimeUrl="/api/copilotkit">
+          <ThemeProvider>{children}</ThemeProvider>
+          <CopilotSideBarComponent />
+        </CopilotKit>
       </main>
     </div>
   );
